fe268b45908113fdba297293a7078fc1b273b122
wyldrodney <wyldrodney@gmail.com>
2012-07-22 17:11:43 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rejuick::Application.config.secret_token = '897659ce441bf1753355014382a104bde092e1b3901a6720dfe365d6a6bbfcd53185c6d1b74cf40a630143d4b234896ed9a8dcca4899f56352426c4827776463'
